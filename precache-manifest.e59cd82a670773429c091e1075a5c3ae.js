e5116380f8de8da6978a3b0daa498fe301ea294d
Tripp H <capehenrykid@gmail.com>
2020-04-06 15:47:50 -0400

    Updates

---
+++ precache-manifest.e59cd82a670773429c091e1075a5c3ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0182630319e5e3a968088be005b4b395",
+    "url": "/GramsPerMoleCalc/index.html"
+  },
+  {
+    "revision": "b53d3029e7d4140456fb",
+    "url": "/GramsPerMoleCalc/static/css/main.00815b3f.chunk.css"
+  },
+  {
+    "revision": "61cd3f1e8c9a3fccc88b",
+    "url": "/GramsPerMoleCalc/static/js/2.be14a755.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/GramsPerMoleCalc/static/js/2.be14a755.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53d3029e7d4140456fb",
+    "url": "/GramsPerMoleCalc/static/js/main.c51d86c4.chunk.js"
+  },
+  {
+    "revision": "d70f050f22885d66af76",
+    "url": "/GramsPerMoleCalc/static/js/runtime-main.08022ef8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GramsPerMoleCalc/static/media/logo.5d5d9eef.svg"
+  }
+]);
